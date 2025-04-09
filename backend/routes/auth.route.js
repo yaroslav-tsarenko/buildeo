7942fecec49cbe0c4991af9583bea1df12f0c8c1
Yaroslav Tsarenko <71490962+yaroslav-tsarenko@users.noreply.github.com>
2025-04-09 04:28:19 +0300

    Delete backend directory

--- backend/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {register, login} = require('../controllers/auth.controller');
-
-router.post('/sign-up', register);
-router.post('/sign-in', login);
-
-module.exports = router;
--- backend/routes/chat.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { createChat, getChats } = require('../controllers/chat.controller');
-const router = express.Router();
-
-router.post('/create', createChat);
-router.get('/get-chats', getChats);
-module.exports = router;
